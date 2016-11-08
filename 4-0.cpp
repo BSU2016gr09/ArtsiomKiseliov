3d55c42f66a549966a956f56ec4375caf3278040
Artsiom Kiseliou <akr1996@mail.ru>
2016-11-08 21:25:24 +0300

    Add files via upload

---
+++ 4-0.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <iomanip> 
+void drawSin()
+{
+	float x, y;
+	for (x = -6; x <= 6; y++)
+	{
+		y = ((sin(2 * (x * 3.14 / 1.5)) * 8) / x * 2);
+		std::cout << std::setw(40 + (y)) << std::setfill(' ') << '*' << "\n";
+		x = x + 0.1;
+		if (x == 0)
+		{
+			x = x + 5;
+		}
+	}
+}
+int main()
+{
+	drawSin();
+	system("pause");
+}
